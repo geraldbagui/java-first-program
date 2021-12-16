78b0c94154dd5764026771970b81c00a28d453fd
Steve Jones <joneswiredbraincoffee@gmail.com>
2021-12-16 14:35:36 +0800

    First Java

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,15 +12,12 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        // do it in module02
-        // todo: fix the implementation
-        // return -1;
         return 2 * number;
     }
 
     private static int add(int[] numbers) {
         var sum = 0;
-        for (int number : numbers) {
+        for(int number: numbers) {
             sum += number;
         }
         return sum;
